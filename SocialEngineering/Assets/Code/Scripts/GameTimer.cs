71c24dde9c43ed7a90d3085c0ca4c3b2139aa7cd
vtullo03 <vitotullo430@gmail.com>
2024-09-28 23:05:27 -0400

    circular timer

---
+++ SocialEngineering/Assets/Code/Scripts/GameTimer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    private Image circle;
+
+    private float duration = 60f;
+    private float remainingDuration;
+
+    private bool timerDone;
+
+    /**
+    * Resets the remaining time variable and flag for the timer
+    * 
+    * @param void
+    * @return void
+    */
+    public void ResetTimer()
+    {
+        timerDone = false;
+        remainingDuration = duration;
+        StartCoroutine(UpdateTimer());
+    }
+
+    /**
+    * Counts down by a sec each and calculates the remaining time fraction for the circle fill
+    * 
+    * @param void
+    * @return void
+    */
+    private IEnumerator UpdateTimer()
+    {
+        while (remainingDuration >= 0)
+        {
+            circle.fillAmount = Mathf.InverseLerp(0, duration, remainingDuration);
+            remainingDuration--;
+            yield return new WaitForSeconds(1f);
+        }
+        timerDone = true;
+    }
+
+    /**
+    * Get the circle UI component and immediately start the game timer
+    * 
+    * @param void
+    * @return void
+    */
+    void Start()
+    {
+        circle = GetComponent<Image>();
+        ResetTimer();
+    }
+
+    /* Getter function */
+    public bool GetTimerStatus() { return timerDone; }
+}
